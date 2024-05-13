cc94a1860c328886b4368128cbb96128d5b2217d
Hidayat Nur Ali Razzaq <91576030+hidrozz@users.noreply.github.com>
2024-05-13 20:56:31 +0700

    Update cowrie-automation.sh

--- cowrie-automation.sh
+++ cowrie-automation.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
 
-echo "updating system"
-sudo su
-apt update
-apt upgrade -y
+# Step 0: Update and Upgrade
+echo "Updating and upgrading system packages..."
+sudo apt-get update && sudo apt-get upgrade -y
 
-#install packages
+# Step 1: Install system dependencies
+echo "Installing system dependencies..."
+sudo apt-get install -y git python3-virtualenv libssl-dev libffi-dev build-essential libpython3-dev python3-minimal authbind virtualenv
 
-echo "installing packages"
+# Step 2: Create a user account
+echo "Creating user account..."
+sudo adduser --disabled-password cowrie
 
-apt install \
-git\
-python3-virtualenv\
-libssl-dev\
-libffi-dev\
-build-essential\
-libpython3-dev\
-python3-minimal\
-authbind\
-virtualenv\
--y
+# Step 3: Checkout the code
+echo "Checking out the code..."
+sudo -u cowrie git clone http://github.com/cowrie/cowrie /home/cowrie/cowrie
 
-echo "downloading cowrie"
-git clone http://github.com/cowrie/cowrie
+# Step 4: Setup Virtual Environment
+echo "Setting up virtual environment..."
+sudo -u cowrie bash -c 'cd /home/cowrie/cowrie && python3 -m venv cowrie-env && source cowrie-env/bin/activate && python -m pip install --upgrade pip && python -m pip install --upgrade -r requirements.txt'
 
-cd cowrie || exit
+# Step 5: Start Cowrie
+echo "Starting Cowrie..."
+sudo -u cowrie bash -c 'cd /home/cowrie/cowrie && source cowrie-env/bin/activate && bin/cowrie start'
 
-# Create and activate virtual environment
-echo "Setting up virtual environment"
-python3 -m venv cowrie-env
-source cowrie-env/bin/activate
-
-# Update pip and install requirements
-echo "Updating pip and installing cowrie requirements"
-python -m pip install --upgrade pip
-python -m pip install --upgrade -r requirements.txt
-
-# Start cowrie
-echo "Starting cowrie"
-bin/cowrie start
+echo "Installation and startup complete."
