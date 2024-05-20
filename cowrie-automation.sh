dff7aacef3861172ba28c7a1f5750f3dedaac338
Hidayat Nur Ali Razzaq <91576030+hidrozz@users.noreply.github.com>
2024-05-20 20:25:10 +0700

    Update cowrie-automation.sh

--- cowrie-automation.sh
+++ cowrie-automation.sh
@@ -33,3 +33,42 @@ echo "Installation and startup complete."
 # Optional: Display Cowrie log
 echo "Displaying Cowrie log..."
 sudo -u cowrie tail -n 10 /home/cowrie/cowrie/var/log/cowrie/cowrie.log
+
+# Install Prometheus Node Exporter
+echo "Installing Prometheus Node Exporter..."
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.0/node_exporter-1.8.0.linux-arm64.tar.gz
+tar xvfz node_exporter-1.8.0.linux-arm64.tar.gz
+sudo mv node_exporter-1.8.0.linux-arm64/node_exporter /usr/local/bin/
+
+# Create node_exporter user
+echo "Creating node_exporter user..."
+sudo useradd -rs /bin/false node_exporter
+
+# Create systemd service file for node_exporter
+echo "Creating systemd service file for Node Exporter..."
+sudo tee /etc/systemd/system/node_exporter.service<<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start and enable node_exporter service
+echo "Starting and enabling Node Exporter service..."
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+
+# Check node_exporter service status
+echo "Checking Node Exporter service status..."
+sudo systemctl status node_exporter
+
+echo "Node Exporter installation and setup complete."
